3501294e1b88e3e4551ab61efca6d5539eaac946
eotsuki <e.otsuki@bizbase.co.jp>
2020-10-10 17:21:11 +0900

    バブルソート処理追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { Binary } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BinarySearchComponent } from './binary-search/binary-search.component';
+import { BubbleSortComponent } from './bubble-sort/bubble-sort.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', pathMatch: 'full', component: BubbleSortComponent },
+  // { path: 'binary', component: BinarySearchComponent },
+  // { path: 'bubble', component: BubbleSortComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/bubble-sort/bubble-sort.component.ts
+++ src/app/bubble-sort/bubble-sort.component.ts
@@ -1,30 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bubble-sort',
+  selector: 'bubble-sort',
   templateUrl: './bubble-sort.component.html',
-  styleUrls: ['./bubble-sort.component.scss']
+  styleUrls: ['./bubble-sort.component.scss'],
 })
+
 export class BubbleSortComponent implements OnInit {
-  nums: number[] = [];
-  num: number = 0;
-  // 81のランダムな3桁の数値の入った配列を作成
-  // 配列の数値をtableに代入
-  onclick() {
+  cell: any;  // 各td要素の値を取得するための変数
+  randam: number; // 乱数のための変数
+
+  // 乱数の作成
+  createRandam() {
     for (let i = 0; i < 81; i++) {
-      this.num = Math.floor(Math.random() * 999);
-      this.nums.push(this.num);
+      this.randam = Math.floor(Math.random() * 999);
+      this.cell = document.getElementById('number' + i);
+      this.cell.innerText = `${this.randam}`;
     }
-    console.log(this.nums);
-
   }
 
-  // ボタンイベントでバブルソート
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  // 全並び替え
+  allBubbleSort() {
+    for (let i = 0; i < 81; i++) {
+      // 移動完了するたびに並び替え対象を縮小するfor文
+      for (let j = 80; j > i; j--) {
+        // ２つの値を比べるためのfor文
+        let target = document.getElementById('number' + j); // 一番後ろの値を取得
+        let comparison = document.getElementById('number' + (j - 1)); // 一番後ろから２番目の値を取得
+        // 比較した値が小さければ入れ替える
+        if (+target.textContent < +comparison.textContent) {
+          let num = target.textContent; // 値を退避させるための変数
+            target.innerText = comparison.textContent;
+            comparison.innerText = num;
+        }
+      }
+    }
+    alert('並べ替えが完了しました！');
   }
 
+  // リセット
+  numberReset() {
+    for (let i = 0; i < 81; i++) {
+      this.randam = Math.floor(Math.random() * 999);
+      this.cell = document.getElementById('number' + i);
+      this.cell.innerText = 0;
+    }
+  }
+
+  // 部分並び替え
+  nums_count: number = 0;
+  nums_compare: number = 80;
+  partBubbleSort() {
+    // 並び替えが１周していたらnums_countを更新
+    if (this.nums_compare === 0) {
+      this.nums_count++;
+      this.nums_compare = 80;
+    }
+    let target = document.getElementById('number' + this.nums_compare);
+    let comparison = document.getElementById('number' + (this.nums_compare - 1));
+    // if (Number(target.textContent) < Number(comparison.textContent)) {
+    if (+target.textContent < +comparison.textContent) { // 「+変数」と「Number(変数)」は同じ意味合いで使用可能
+      let num = target.textContent;
+      target.innerText = comparison.textContent;
+      comparison.innerText = num;
+    }
+    this.nums_compare--;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
